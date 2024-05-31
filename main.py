437fd6eed559106dd6c4886ba92c15a8ea339f1d
Will1v <24796480+Will1v@users.noreply.github.com>
2024-05-31 09:42:44 +0100

    Split out tables + store RTT stats

--- main.py
+++ main.py
@@ -8,27 +8,43 @@ from broadband_monitor.config import config
 import time
 from datetime import datetime
 import sqlite3
+import re
+from typing import Tuple, Union
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 logger = get_logger(__name__)
 
 
-def ping_host(host):
+def ping_host(host: str):
     result = subprocess.run(
         ["ping", "-c", "4", host],
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
         text=True,
     )
-    success = True
     if result.returncode == 0:
-        logger.debug(f"Ping to {host} was successful.")
-        logger.debug(result.stdout)
+        return extract_rtt_stats(result.stdout)
     else:
-        logger.error(f"Ping to {host} failed.")
-        logger.error(result.stderr)
-        return False
-    return True
+        logger.error(
+            f"Ping to {host} failed. \nStdout: \n{result.stdout} \nStderr: \n{result.stderr}"
+        )
+        return None
+
+
+def extract_rtt_stats(
+    ping_output: str,
+) -> Union[Tuple[float, float, float], None]:
+    # Regex pattern to extract min/avg/max/mdev values from ping output
+    pattern = r"(?<=rtt min/avg/max/mdev = )(\d+\.\d+)/(\d+\.\d+)/(\d+\.\d+)/(\d+\.\d+)"
+    match = re.search(pattern, ping_output)
+    if match:
+        min_rtt = float(match.group(1))
+        avg_rtt = float(match.group(2))
+        max_rtt = float(match.group(3))
+        return min_rtt, avg_rtt, max_rtt
+    else:
+        logger.error("Could not parse the ping output.")
+        return None
 
 
 def main():
@@ -38,31 +54,59 @@ def main():
     internet_ok_counter = 0
     router_nok_counter = 0
     internet_nok_counter = 0
-    interval_in_s = 20
-    counter_modulo = 60 * 60 / interval_in_s
-    router_ip, internet_address = config.router_ip, config.internet_address
+    interval_in_s = config.ping_interval_in_seconds
+    counter_modulo = (
+        60 * 60 / interval_in_s
+    )  # Will log a heartbreat with some stats every hour
+    router_ip, router_alias, internet_address, internet_address_alias = (
+        config.router_ip,
+        config.router_alias,
+        config.internet_address,
+        config.internet_address_alias,
+    )
 
     # Init DB
     conn = init_db()
+
+    router_id = register_target(config.router_ip, config.router_alias)
+    internet_id = register_target(
+        config.internet_address, config.internet_address_alias
+    )
 
     logger.info(
         f"Pinging router at {router_ip} and internet address at {internet_address}"
     )
     while True:
         router_status = ping_host(router_ip)
+        # TODO: Duplicate code. Refactor to make just one function to both router and internet (and any other target)
         if router_status:
             router_ok_counter += 1
+            router_min, router_max, router_avg = router_status
         else:
             router_nok_counter += 1
 
         internet_status = ping_host(internet_address)
         if internet_status:
             internet_ok_counter += 1
+            internet_min, internet_max, internet_avg = internet_status
         else:
             internet_nok_counter += 1
 
         counter += 1
-        register_status_to_db(router_status, internet_status)
+        register_status_to_db(
+            True if router_status else False,
+            router_min,
+            router_max,
+            router_avg,
+            router_id,
+        )
+        register_status_to_db(
+            True if internet_status else False,
+            internet_min,
+            internet_max,
+            internet_avg,
+            internet_id,
+        )
 
         if (counter - 1) % counter_modulo == 0:
             logger.info(
@@ -71,10 +115,37 @@ def main():
         time.sleep(interval_in_s)
 
 
-def register_status_to_db(router_status, internet_status):
+def register_target(target: str, target_alias: str) -> int:
     conn = get_db_connection()
-    sql_query = "INSERT INTO ping_results (timestamp, router_success, internet_success) VALUES (?, ?, ?)"
-    conn.cursor().execute(sql_query, [datetime.now(), router_status, internet_status])
+    get_id_sql_query = "SELECT target_id FROM targets WHERE ip_or_url = ?"
+    res = conn.cursor().execute(get_id_sql_query, [target]).fetchone()
+    if not res:
+        targets_sql_query = (
+            "INSERT OR REPLACE INTO targets (ip_or_url, alias) VALUES (?, ?)"
+        )
+        conn.cursor().execute(targets_sql_query, [target, target_alias])
+        conn.commit()
+        res = conn.cursor().execute(get_id_sql_query, [target]).fetchone()
+    return res[0]
+
+
+def register_status_to_db(
+    target_status: bool,
+    rtt_min: Union[float, None],
+    rtt_max: Union[float, None],
+    rtt_avg: Union[float, None],
+    target_id: int,
+):
+    conn = get_db_connection()
+    # Insert ping status
+    sql_status_query = (
+        "INSERT INTO ping_results (timestamp, target_id, success) VALUES (?, ?, ?)"
+    )
+    conn.cursor().execute(sql_status_query, [datetime.now(), target_id, target_status])
+    sql_stats_query = "INSERT INTO ping_stats (timestamp, target_id, rtt_min, rtt_max, rtt_avg) VALUES (?, ?, ?, ?, ?)"
+    conn.cursor().execute(
+        sql_stats_query, [datetime.now(), target_id, rtt_min, rtt_max, rtt_avg]
+    )
     conn.commit()
 
 
